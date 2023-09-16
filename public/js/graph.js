753bec3c52e87b57fc29392ce97a86958e13f1ee
Ridwanullahi-super <asimiyuridwan5@gmail.com>
2023-09-16 16:08:50 +0100

    css and js

---
+++ public/js/graph.js
@@ -0,0 +1,41 @@
+const ctx = document.getElementById('myAreaChart');
+            
+new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+const ct = document.getElementById('myPieChart');
+            
+new Chart(ct, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+}); 
